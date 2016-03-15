3728f9e6e65d682c9f2d3e8bab14467214d65879
Otavio Ap. Leonardo <otavio.leonardo@campinas.sp.gov.br>
2016-03-15 16:44:02 -0300

    Criado  arquivos   29/02/2016 - 10:40 Alterado  arquivos 15/03/2016 - 10:40

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php
+$rota = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+$path_ = substr($rota['path'],1);
+$path = explode('/',$path_);
+$path = str_replace(".php", "", $path[1]);
+
+if(empty($path))
+   $path = "index";
+
 require_once 'menu.php';
 require_once 'footer.php';
 ?>
 <section>
     <div class="row">
-        <?php require_once 'menu.php'; ?>
+<!--        <?php require_once ("menu.php"); ?>
+    </div>         
+    <div>-->
+        <?php // require_once ($path.".php"); ?>
+        <?php require_once ($path.".php"); ?>
     </div>         
     <div class="row">
         <div class="col-md-4">
--- produtos.php
+++ produtos.php
@@ -4,19 +4,46 @@ require_once 'footer.php';
 $page = $_REQUEST['page'];
 ?>
 <h4><?php echo $page; ?></h4>
-<div>
-    <ul class="list-group">
-        <li><a href="">KIT PORTÃO DESLIZANTE CONDOMÍNIO</a></li>
-        <li><a href="">FIOS E CABOS</a></li>
-        <li><a href="">GRAVADOR TELEFÔNICO</a></li>
-        <li><a href="">INTERFONE</a></li>
-        <li><a href="">ALARMES</a></li>
-        <li><a href="">CÂMERAS PARA CFTV</a></li>
-        <li><a href="">KIT PORTÃO BASCULANTE CONDOMÍNIO</a></li>
-        <li><a href="">KIT PORTÃO PIVOTANTE CONDOMÍNIO</a></li>
-        <li><a href="">CANCELA ELETRÔNICA BAIXO FLUXO</a></li>
-        <li><a href="">CANCELA ELETRÔNICA  ALTO FLUXO</a></li>
-    </ul>
+<div class="container">
+    <table  class="table" border="0" width="700">
+        <tr>
+            <td>
+                <div>
+                    <ul class="list-group">
+                        <li><a href="">KIT PORTÃO DESLIZANTE CONDOMÍNIO</a></li>
+                        <li><a href="">FIOS E CABOS</a></li>
+                        <li><a href="">GRAVADOR TELEFÔNICO</a></li>
+                        <li><a href="">INTERFONE</a></li>
+                        <li><a href="">ALARMES</a></li>
+                        <li><a href="">CÂMERAS PARA CFTV</a></li>
+                        <li><a href="">KIT PORTÃO BASCULANTE CONDOMÍNIO</a></li>
+                        <li><a href="">KIT PORTÃO PIVOTANTE CONDOMÍNIO</a></li>
+                        <li><a href="">CANCELA ELETRÔNICA BAIXO FLUXO</a></li>
+                        <li><a href="">CANCELA ELETRÔNICA  ALTO FLUXO</a></li>
+                    </ul>
+                </div>
+            </td>
+            <td>
+                <div class="row">
+                    <div class="col-md-2">
+                        <img src="portao_1.jpg" class="img-responsive" alt="Cinque Terre" style="width:350px;height:100px"> 
+                    </div>         
+                    <div class="col-md-2">
+                        <img src="portao_2.jpg" class="img-responsive" alt="Cinque Terre" style="width:350px;height:100px"> 
+                    </div>         
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-md-2">
+                        <img src="portao_3.jpg" class="img-responsive" alt="Cinque Terre" style="width:350px;height:100px"> 
+                    </div>         
+                    <div class="col-md-2">
+                        <img src="portao_4.jpg" class="img-responsive" alt="Cinque Terre" style="width:350px;height:100px"> 
+                    </div>         
+                </div>         
+            </td>
+        </tr>
+    </table>
 </div>
 <?php
 showFooter($botao = 0);
