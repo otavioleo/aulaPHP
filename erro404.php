ec035a0a8a74bf130a89014ba8a690ae70c11bd2
Otavio Ap. Leonardo <otavio.leonardo@campinas.sp.gov.br>
2016-03-31 11:32:55 -0300

    Criado  arquivos   29/02/2016 - 10:40 Alterado  arquivos 15/03/2016 - 10:40 Alterado  arquivos 31/03/2016 - 11:30

---
+++ erro404.php
@@ -0,0 +1,9 @@
+<?php
+header("HTTP/1.0 404 Not Found");
+?>
+<div id="page">
+    <h1>Página não encontrada</h1>
+    <p>Pedimos desculpa, mas a página que acessou não está mais disponível! Poderá ter sido removida ou alterada.</p>
+    <p>Já verificou na barra de endereço do seu browser de internet se o URL está correto?.</p>
+    <p>Se desejar, poderá ir para a nossa <a href="index.php">home page</a></p>
+</div>
--- index.php
+++ index.php
@@ -1,38 +1,35 @@
 <?php
-$rota = parse_url("http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-$rota_ = dirname(__FILE__) . "/";
-$path_ = substr($rota['path'], 1);
-$path = explode('/', $path_);
-$path = str_replace(".php", "", $path[1]);
-if (empty($path)) {
-   $path = "index";
-}
-$path = $path . ".php";
-//echo $path; die();
-if (file_exists($rota_ . $path)) {
-   require_once 'menu.php';
-   require_once 'footer.php';
-   ?>
-   <section>
-       <div class="row">
-           <?php require_once($path); ?>
-       </div>
-       <?php if ($path == 'index.php') { ?>
-          <div class="row">
-              <div class="col-md-4">
-                  <img src="logo.jpg" class="img-responsive" alt="Cinque Terre" style="width:350px;height:250px"> 
-              </div>         
-          </div>         
-       <?php } ?>
-   </section>
-   <?php
-} else {
-   ?>   
+$rotas = ['index', 'home', 'empresa', 'servicos', 'contato'];
+$url = parse_url("http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+$url = substr($url['path'], 1);
+$url = explode('/', $url);
+$url = $url[1];
+$url = str_replace(".php", "", $url);
 
-   <div id="page">
-       <h1>Página não encontrada</h1>
-       <p>Pedimos desculpa, mas a página que acessou não está mais disponível! Poderá ter sido removida ou alterada.</p>
-       <p>Já verificou na barra de endereço do seu browser de internet se o URL está correto?.</p>
-       <p>Se desejar, poderá ir para a nossa <a href="index.php">home page</a></p>
-   </div>
-<?php } ?>
+if (empty($url)) {
+   $url = "index";
+}
+
+if (in_array($url, $rotas)) {
+   if (file_exists($url . ".php")) {
+      $pagina = $url . ".php";
+      require_once 'menu.php';
+      require_once 'footer.php';
+   } else {
+      $pagina = "erro404.php";
+   }
+} else {
+   $pagina = "erro404.php";
+}
+
+require_once($pagina);
+
+if ($pagina == 'index.php') {
+   ?>
+   <div class="row">
+       <div class="col-md-4">
+           <img src="logo.jpg" class="img-responsive" alt="Cinque Terre" style="width:350px;height:250px"> 
+       </div>         
+   </div>         
+   <?php
+}
